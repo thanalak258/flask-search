a7112ab204845176379ff7d64cd6917c21b71171
thanalak258 <6210742166@student.tu.ac.th>
2022-07-26 10:20:59 +0700

    delete libary

--- app.py
+++ app.py
@@ -1,7 +1,5 @@
 from flask import Flask, render_template, redirect, request, url_for, session, flash
 from flask_mysqldb import MySQL, MySQLdb
-import bcrypt
-from numpy import dtype
 import pandas
 from datetime import date
 import glob
